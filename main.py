2f0467cc46c15f11acc0dd41c0b167dad7cd8214
Nicole Pedayev <144907213+Nicoly-P@users.noreply.github.com>
2023-09-18 16:54:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import game_field as gf
+
 pygame.init()
 
 size_x = 1000
@@ -13,23 +15,36 @@ pygame.display.flip()
 while size_x != 1:
     screen
 
+soldier = pygame.Rect(20,40,40,80)
 
 def main():
     running = True
     while running:
-        def handle_user_events():
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_KP_ENTER:
-                        pass  # show landmine screen
-                    if event.key == pygame.K_UP:
-                        pass  # move up
-                    if event.key == pygame.K_DOWN:
-                        pass  # move down
-                    if event.key == pygame.K_LEFT:
-                        pass  # move left
-                    if event.key == pygame.K_RIGHT:
-                        pass  # move right
-                if event.type == pygame.QUIT:
-                    running = False  # game loop 'while running'
 
+        pygame.draw.rect(screen, (255,0,0), soldier)
+
+        handle_user_events()
+
+        pygame.display.update()
+    pygame.quit()
+
+main()
+
+
+
+
+def handle_user_events():
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_KP_ENTER:
+                pass  # show landmine screen
+            if event.key == pygame.K_UP:
+                pass
+            if event.key == pygame.K_DOWN:
+                pass  # move down
+            if event.key == pygame.K_LEFT:
+                pass  # move left
+            if event.key == pygame.K_RIGHT:
+                pass  # move right
+        if event.type == pygame.QUIT:
+            running = False  # game loop 'while running'
